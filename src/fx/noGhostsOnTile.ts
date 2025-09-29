e5682acb343819da8fa695a3bf370db14436abe2
tygrysek90 <risa.szlachta@gmail.com>
2025-09-29 19:46:40 +0200

    Optimize a function

--- src/fx/noGhostsOnTile.ts
+++ src/fx/noGhostsOnTile.ts
@@ -13,15 +13,10 @@
  * @returns true if there are elements with ghost flag on given tile
  */
 export function noGhostsOnTile(tile: Tile): boolean {
-    // TODO přepsat za použití "for" nebo "do" cyklu s vyskočením přes return (nebo break???)
-    let seenGhost = false;
-    tile.elements.forEach(element => {
-        if (element.isGhost) {
-            seenGhost = true;
+    for (let i=0; i<tile.numElements; i++) {
+        if (tile.elements[i].isGhost) {
+            return false
         }
-        else {
-            seenGhost = false;
-        }
-    });
-    return !seenGhost;
+    }
+    return true
 }
