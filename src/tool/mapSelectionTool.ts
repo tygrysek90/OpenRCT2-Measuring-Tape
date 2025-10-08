f81c9122390c3b6c0eb6dd2c46350b4a83a79f8b
tygrysek90 <risa.szlachta@gmail.com>
2025-10-08 18:45:25 +0200

    Remove water selection and caused jitter

--- src/tool/mapSelectionTool.ts
+++ src/tool/mapSelectionTool.ts
@@ -89,7 +89,7 @@ export class MapSelectionTool
 		ui.activateTool({
 			id: this.name,
 			cursor: this.cursor,
-			filter: ["terrain", "water"],
+			filter: ["terrain"],
 			onDown: a => down(this, a),
 			onUp: a => up(this, a),
 			onMove: a => move(this, a),
