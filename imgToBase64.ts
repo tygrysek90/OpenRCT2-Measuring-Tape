899193098c2336f120a4a133a9e6dfdd5df82017
tygrysek90 <risa.szlachta@gmail.com>
2025-10-11 18:19:13 +0200

    Spice graphics packing script

--- imgToBase64.ts
+++ imgToBase64.ts
@@ -1,4 +1,6 @@
 import fs from "fs"
+import chalk from 'chalk';
+
 
 const inDir = "./img/"
 const outFile = "./src/graphics/pluginGraphics.ts"
@@ -10,10 +12,16 @@ output = `import { createImageFromBase64 } from "../fx/createImageFromBase64"\n`
 output += `export const pluginGraphics = {\n`
 
 files.forEach(file => {
-    output += `\t${file.slice(0,-4)}: createImageFromBase64("${(fs.readFileSync(`${inDir}/${file}`).toString("base64"))}"),\n`
+    if (file.length > 4 && file.slice(-3) == "png") {
+        output += `\t${file.slice(0,-4)}: createImageFromBase64("${(fs.readFileSync(`${inDir}/${file}`).toString("base64"))}"),\n`
+        console.log(chalk.cyanBright("Processed " + chalk.bold(`${inDir}${file}`)))
+    }
+    else {
+        console.log(chalk.magentaBright(`Skipping ${inDir}${file}`))
+    }
 });
 output += `}\n`
 
 fs.writeFileSync(outFile, output)
 
-console.log(`Graphics to base64 done: ${files.length} files packed into "${outFile}"`)
+console.log(chalk.bold(chalk.greenBright(`Graphics to base64 done: ${files.length} files packed into "${outFile}"\n`)))
